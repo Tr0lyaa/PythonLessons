c5d26bce6b3ff1f8772f733030412617ea1b3f8b
Tr0lyaa <tolik2501@gmail.com>
2024-05-20 14:03:26 +0500

    Lesson11 ver2

--- Lesson11/main.py
+++ Lesson11/main.py
@@ -1,10 +1,7 @@
 cars = ["BMW", "MB", "LADA", "KIA", "HONDA"]
-for i in cars:
-    print("Я езжу на автомобиле марки " + i)
-print("")
-
 cars_count = 0
+
 for i in cars:
     print("Я езжу на автомобиле марки " + i)
     cars_count += 10
-    print(cars_count)
+print(cars_count)
