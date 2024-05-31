c4f39dbd7d7e8fe5c5342a2b9ab2c20fec1e478f
Tr0lyaa <tolik2501@gmail.com>
2024-06-01 02:58:14 +0500

    Module4 1

--- Lesson18/fake_math.py
+++ Lesson18/fake_math.py
@@ -1,2 +1,2 @@
-def divide(num1, num2):
-    return num1 / num2 if num2 != 0 else 'Ошибка'
+def divide(first, second):
+    return first / second if second != 0 else 'Ошибка'
--- Lesson18/true_math.py
+++ Lesson18/true_math.py
@@ -1,4 +1,4 @@
 from math import inf
 
-def divide(num1, num2):
-    return num1 / num2 if num2 != 0 else inf
+def divide(first, second):
+    return first / second if second != 0 else inf
