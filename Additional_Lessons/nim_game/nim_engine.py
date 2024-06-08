ba2b7105f6b8f1a9f70804e895a9a44663daeba2
Tr0lyaa <tolik2501@gmail.com>
2024-06-08 08:16:10 +0500

    Вторая версия игры Ним.

--- Additional_Lessons/nim_game/nim_engine.py
+++ Additional_Lessons/nim_game/nim_engine.py
@@ -1,26 +1,45 @@
 from random import randint
-from termcolor import colored
+from termcolor import cprint, colored
 
-MAX_BUNCHES = 5
-_holder = []
+MAX_BUNCHES = 10
+MAX_BUNCHES_SIZE = 20
+_holder = {}
 
 def put_stones():
     global _holder
-    _holder = []
-    bunches = input(colored(f'Введите число куч от 1 до {MAX_BUNCHES}: ', color = 'green', force_color='True'))
+    _holder = {}
+    while True:
+        bunches = input(colored(f'Введите число куч от 3 до {MAX_BUNCHES}: ', color = 'green', force_color='True'))
+        if bunches.isdigit() and 3 <= int(bunches) <= MAX_BUNCHES:
+            for i in range(1, int(bunches) + 1):
+                _holder[i] = randint(1, 20)
+            break
+        else:
+            cprint('Ошибка ввода!', 'red', force_color='True')
 
-    for i in range(int(bunches)):
-        _holder.append(randint(1, 20))
+def check_pos(pos):
+    if pos.isdigit() and int(pos) in _holder and _holder[int(pos)] >= 1:
+        return True
+    else:
+        cprint('Ошибка ввода!', 'red', force_color='True')
+        return False
 
+def check_qua(pos, qua):
+    if qua.isdigit() and 1 <= int(qua) <= _holder[int(pos)]:
+        return True
+    else:
+        cprint('Ошибка ввода!', 'red', force_color='True')
+        return False
 
 def take_from_bunch(position, quantity):
-    if 1 <= position <= len(_holder):
-        _holder[position - 1] -= quantity
-
+    if position in _holder:
+        _holder[position] -= quantity
 
 def get_bunches():
-    return _holder
-
+    res = []
+    for key in sorted((_holder.keys())):
+        res.append(_holder[key])
+    return res
 
 def is_gameover():
-    return sum(_holder) == 0
+    return sum(_holder.values()) == 0
--- Additional_Lessons/nim_game/nim_main.py
+++ Additional_Lessons/nim_game/nim_main.py
@@ -1,4 +1,4 @@
-from nim_engine import get_bunches, put_stones, is_gameover, take_from_bunch
+from nim_engine import get_bunches, put_stones, is_gameover, take_from_bunch, check_pos, check_qua
 from termcolor import cprint, colored
 
 put_stones()
@@ -10,8 +10,15 @@ while True:
     user_color = 'blue' if user_number == 1 else 'yellow'
     cprint(f'Ход игрока номер {user_number}', user_color, force_color='True')
 
-    pos = input(colored('Откуда берём: ', user_color, force_color='True'))
-    qua = input(colored('Сколько берём: ', user_color, force_color='True'))
+    while True:
+        pos = input(colored('Откуда берём: ', user_color, force_color='True'))
+        if check_pos(pos):
+            break
+
+    while True:
+        qua = input(colored('Сколько берём: ', user_color, force_color='True'))
+        if check_qua(pos, qua):
+            break
 
     take_from_bunch(int(pos), int(qua))
 
