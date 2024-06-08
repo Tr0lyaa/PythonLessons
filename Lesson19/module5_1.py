1493f656fdbb1714b4a57814d8b10c69acc0741a
Tr0lyaa <tolik2501@gmail.com>
2024-06-08 06:26:08 +0500

    Практическое занятие по теме "Пространство имен."

---
+++ Lesson19/module5_1.py
@@ -0,0 +1,10 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    print("Я test_function")
+    inner_function()
+
+# inner_function() -> NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+
+test_function()
