64e6e2e9ce87cc9610722cfb5c2ccf41eba37aeb
Tr0lyaa <tolik2501@gmail.com>
2024-06-10 22:31:17 +0500

    Четвёртая версия игры Ним.

--- Additional_Lessons/nim_game/nim_engine.py
+++ Additional_Lessons/nim_game/nim_engine.py
@@ -9,26 +9,26 @@ def put_stones():
     global _holder
     _holder = {}
     while True:
-        bunches = input(colored(f'Введите число куч от 3 до {MAX_BUNCHES}: ', color = 'green', force_color='True'))
+        bunches = input(colored(f'Введите число куч от 3 до {MAX_BUNCHES}: ', color = 'green'))
         if bunches.isdigit() and 3 <= int(bunches) <= MAX_BUNCHES:
             for i in range(1, int(bunches) + 1):
                 _holder[i] = randint(1, 20)
             break
         else:
-            cprint('Ошибка ввода!', 'red', force_color='True')
+            cprint('Ошибка ввода!', 'red')
 
 def check_pos(pos):
     if pos.isdigit() and int(pos) in _holder and _holder[int(pos)] >= 1:
         return True
     else:
-        cprint('Ошибка ввода!', 'red', force_color='True')
+        cprint('Ошибка ввода!', 'red')
         return False
 
 def check_qua(pos, qua):
     if qua.isdigit() and 1 <= int(qua) <= _holder[int(pos)]:
         return True
     else:
-        cprint('Ошибка ввода!', 'red', force_color='True')
+        cprint('Ошибка ввода!', 'red')
         return False
 
 def take_from_bunch(position, quantity):
--- Additional_Lessons/nim_game/nim_main.py
+++ Additional_Lessons/nim_game/nim_main.py
@@ -7,27 +7,27 @@ put_stones()
 user_number = 1
 
 while True:
-    cprint(f'Количество камней в кучах: {get_bunches()}', 'green', force_color='True')
+    cprint(f'Количество камней в кучах: {get_bunches()}', 'green')
 
     user_color = 'blue' if user_number == 1 else 'yellow'
-    cprint(f'Ход игрока номер {user_number}', user_color, force_color='True')
+    cprint(f'Ход игрока номер {user_number}', user_color)
 
     while True:
-        pos = input(colored('Откуда берём: ', user_color, force_color='True'))
+        pos = input(colored('Откуда берём: ', user_color))
         if check_pos(pos):
             break
 
     while True:
-        qua = input(colored('Сколько берём: ', user_color, force_color='True'))
+        qua = input(colored('Сколько берём: ', user_color))
         if check_qua(pos, qua):
             break
 
     take_from_bunch(int(pos), int(qua))
 
     if is_gameover():
-        cprint(f'\nВыиграл игрок номер {user_number}! Поздравляем!!!\n', user_color, force_color='True')
+        cprint(f'\nВыиграл игрок номер {user_number}! Поздравляем!!!\n', user_color)
 
-        if input(colored('Если хотите сыграть напишите 1: ', 'green', force_color='True')) == '1':
+        if input(colored('Если хотите сыграть напишите 1: ', 'green')) == '1':
             put_stones()
             user_number = 1
             continue
