85be5e3f3c97c2f27d4866106732e87e241cef63
Tr0lyaa <tolik2501@gmail.com>
2024-06-12 13:33:56 +0500

    Вводное практическое занятие по курсу "Python-Разработчик". (Новое)

---
+++ Lesson0/main2.py
@@ -0,0 +1,24 @@
+a = 'Hello'
+print(len(a))
+
+first = 10
+second = 20
+summa = first + second
+diff = first - second
+print(summa, diff)
+
+first = 3
+second = 6
+third = 9
+mean = (first + second + third) / 3
+print(mean)
+
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string + ', ' + first_string)
+
+a = 2
+b = 3
+c = 4
+f = (((a * b) + (a * c)) ** 3) / 2
+print(f)
