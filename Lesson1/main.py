f42936732e6dc4246fca7da02f8b2df625235d57
Tr0lyaa <tolik2501@gmail.com>
2024-06-12 14:12:22 +0500

    Практическое занятие по теме "Строки и индексация строк". (Новое)

--- Lesson1/main.py
+++
@@ -1,7 +0,0 @@
-line = 'это моя строка'
-print(line[0])                       # 1 задание
-print(line[-1])                      # 2 задание
-print(line[2:5])                     # 3 задание
-print(line[::-1])                    # 4 задание
-print(len(line))                     # 5 задание
-print('это новая строка' + line)     # 6 задание
