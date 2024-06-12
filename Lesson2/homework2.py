af0a039c13f8014372aea03ba3480cc668f67f2d
Tr0lyaa <tolik2501@gmail.com>
2024-06-12 13:51:13 +0500

    Практическое занятие по теме "Переменные". (Новое)

---
+++ Lesson2/homework2.py
@@ -0,0 +1,6 @@
+homeworks = 12
+time = 1.5
+name = 'Python'
+avg_time = time / homeworks
+
+print('Курс: ', name, ', всего задач: ', homeworks, ', затрачено часов: ', time, ', среднее время выполнения ', avg_time, ' часа.' , sep='')
