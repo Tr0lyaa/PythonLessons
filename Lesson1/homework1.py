90e65f5d8b4497c8653d5da4c5bbd062ebfaafcf
Tr0lyaa <tolik2501@gmail.com>
2024-06-12 13:21:54 +0500

    Практическое занятие по теме "Строки и индексация строк". (Новое).

---
+++ Lesson1/homework1.py
@@ -0,0 +1,6 @@
+line = 'Топинамбур'
+print(line[0])                       # 1 задание
+print(line[-1])                      # 2 задание
+print(line[int(len(line) / 2):])     # 3 задание
+print(line[::-1])                    # 4 задание
+print(line[1::2])                     # 5 задание
