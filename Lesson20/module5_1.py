948da09cdd3328dc16b86bd991b0aed1586c3845
Tr0lyaa <tolik2501@gmail.com>
2024-06-12 12:50:42 +0500

    Практическое занятие по теме "Атрибуты и методы объекта."

--- Lesson20/module5_1.py
+++ Lesson20/module5_1.py
@@ -8,10 +8,14 @@ class House:
         if 1 <= new_floor <= self.number_of_floors:
             for i in range(1, new_floor + 1):
                 print(i)
+            print()
         else:
-            print('Такого этажа не существует!')
+            print('Такого этажа не существует!', end='\n\n')
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
-h1.go_to(5)
-h2.go_to(10)
+h1.go_to(20)
+h1.go_to(1)
+h1.go_to(15)
+h2.go_to(-1)
+h2.go_to(2)
