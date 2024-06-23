d1ea6603fa2ad760f237d34ce214b168f15127fa
Tr0lyaa <tolik2501@gmail.com>
2024-06-23 17:19:38 +0500

    Практическое занятие по теме "Множественное наследование."

--- Lesson027/module6_3.py
+++ Lesson027/module6_3.py
@@ -1,9 +1,16 @@
 class Vehicle:
     vehicle_type = None
+    
+    def __init__(self,vehicle_type, price):
+        self.vehicle_type = vehicle_type
+        super().__init__(price)
 
 class Car:
     price = 1000000
 
+    def __init__(self, price):
+        self.price = price
+    
     def horse_powers(self):
         print('Я из класса Car, мощность двигателя:', end=' ')
         return self.power
@@ -16,8 +23,7 @@ class Nissan(Vehicle, Car):
     power = 500
 
     def __init__(self, vehicle_type, price):
-        self.vehicle_type = vehicle_type
-        self.price = price
+        super().__init__(vehicle_type, price)
 
     def horse_powers(self):
         print('Я из класса Nissan, мощность двигателя:', end=' ')
