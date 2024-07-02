239c122eac11998de4df6c6514b6f284b2d9c1e3
tr0lyaa <tolik2501@gmail.com>
2024-07-03 04:06:59 +0500

    Практическое занятие по теме "Режимы открытия файлов."

---
+++ Lesson029/module7_1.py
@@ -0,0 +1,42 @@
+class Product:
+
+    def __init__(self, name, weight, category):
+        self.name = name
+        self.weight = weight
+        self.category = category
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+
+class Shop:
+
+    __file_name = 'products.txt'
+
+    def get_products(self):
+        file = open(self.__file_name, 'r')
+        products = file.read()
+        file.close()
+        return products
+
+    def add(self, *products):
+        for product in products:
+            _products = self.get_products()
+            if product.name in _products:
+                print(f'Продукт {product.name} уже есть в магазине')
+            else:
+                file = open(self.__file_name, 'a')
+                file.write(f'{str(product)}\n')
+                file.close()
+
+
+shop = Shop()
+p1 = Product('Potato', 50.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.4, 'Groceries')
+p3 = Product('Potato', 5.5, 'Vegetables')
+
+print(p2)
+
+shop.add(p1, p2, p3)
+
+print(shop.get_products())
