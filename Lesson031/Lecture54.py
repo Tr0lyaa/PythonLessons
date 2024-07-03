bdc47461cc4b154a7444e2d54dcf0d9d1ef2dd05
tr0lyaa <tolik2501@gmail.com>
2024-07-03 05:42:04 +0500

    Лекция по теме "Оператор "with"."

---
+++ Lesson031/Lecture54.py
@@ -0,0 +1,11 @@
+from pprint import pprint
+
+name = 'sample.txt'
+# Оператор with позволяет создать блок кода, в котором будет существовать указанный объект.
+# В случае файлов, их можно не закрывать вручную.
+with open(name, encoding='utf-8') as file:
+    for line in file:
+        for char in line:
+            print(char, end='')
+    print()
+    print(file.tell())
