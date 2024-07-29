776a1018335bdae1356b77c9def26d4b3e97d286
tr0lyaa <tolik2501@gmail.com>
2024-07-29 08:41:56 +0500

    Практическое занятие по теме "Потоки на классах".

--- Lesson045/module10_2.py
+++ Lesson045/module10_2.py
@@ -10,12 +10,11 @@ class Knight(Thread):
         self.enemy_count = enemy_count
         self.days = 0
 
-
     def run(self):
         print(f'{self.name}, на нас напали!')
         while self.enemy_count > 0:
             self.days += 1
-            self.enemy_count -= self.power
+            self.enemy_count = self.enemy_count - self.power if self.enemy_count >= self.power else 0
             print(f'{self.name}, сражается {self.days} дней(дня), осталось {self.enemy_count}')
             sleep(1)
         print(f'{self.name} одержал победу спустя {self.days} дней(дня)')
