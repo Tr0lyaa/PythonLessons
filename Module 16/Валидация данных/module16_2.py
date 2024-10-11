449b0878c8102148dd2438cc59c26c4dd5c0aebe
tr0lyaa <tolik2501@gmail.com>
2024-10-11 14:19:22 +0500

    Строка 25 -- изменение на Annotated

--- Module 16/Валидация данных/module16_2.py
+++ Module 16/Валидация данных/module16_2.py
@@ -22,5 +22,5 @@ async def user_page(user_id: Annotated[int, Path(ge=1, le=100, description="Ente
 @app.get("/user/{username}/{age}")
 async def user_info_page(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username",
                                                        example="UrbanUser")]
-                         , age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> str:
+                         , age: Annotated[int, Path(ge=18, le=120, description="Enter age", example="24")]) -> str:
     return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
