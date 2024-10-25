554d86014732ef448474ad7ebe778b796be89836
tr0lyaa <tolik2501@gmail.com>
2024-10-25 15:06:34 +0500

    Первый вариант работы

---
+++ Module 14 - Библиотека для работы с данными/2. Выбор элементов и функции в SQL запросах/module_14_2.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute("DELETE FROM Users WHERE id = ?", (6, ))
+cursor.execute("SELECT COUNT(*) FROM Users")
+all_users = cursor.fetchone()[0]
+cursor.execute("SELECT SUM(balance) FROM Users")
+all_balance = cursor.fetchone()[0]
+print(all_balance/all_users)
+
+connection.commit()
+connection.close()
