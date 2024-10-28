f147944388f5dd9ebf2d9d482ade290eb7c9006f
tr0lyaa <tolik2501@gmail.com>
2024-10-28 18:37:40 +0500

    Создана функция initiate_db

--- Module 14 - Библиотека для работы с данными/4. План написания админ панели/Практика/crud_functions.py
+++ Module 14 - Библиотека для работы с данными/4. План написания админ панели/Практика/crud_functions.py
@@ -3,21 +3,16 @@ import sqlite3
 connection = sqlite3.connect("telegram_bot.db")
 cursor = connection.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS Products(
-id INTEGER PRIMARY KEY,
-title TEXT NOT NULL,
-description TEXT,
-price INTEGER NOT NULL
-);
-''')
 
-# names = ["Яблоко", "Банан", "Вишня", "Клубника"]
-# names_desc = ["Спелые яблоки", "Свежие бананы", "Сладкая вишня", "Вкусная клубника"]
-#
-# for i in range(4):
-#     cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)",
-#                    (names[i], names_desc[i], (i + 1) * 100))
+def initiate_db():
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    );
+    ''')
 
 
 def get_all_products():
