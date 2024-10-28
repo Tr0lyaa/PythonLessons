8f40780d9d5438f12b663880f678d391567c43a8
tr0lyaa <tolik2501@gmail.com>
2024-10-28 17:25:15 +0500

    Первый вариант работы

---
+++ Module 14 - Библиотека для работы с данными/4. План написания админ панели/Практика/crud_functions.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connection = sqlite3.connect("telegram_bot.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Products(
+id INTEGER PRIMARY KEY,
+title TEXT NOT NULL,
+description TEXT,
+price INTEGER NOT NULL
+);
+''')
+
+# names = ["Яблоко", "Банан", "Вишня", "Клубника"]
+# names_desc = ["Спелые яблоки", "Свежие бананы", "Сладкая вишня", "Вкусная клубника"]
+#
+# for i in range(4):
+#     cursor.execute("INSERT INTO Products (title, description, price) VALUES (?, ?, ?)",
+#                    (names[i], names_desc[i], (i + 1) * 100))
+
+
+def get_all_products():
+    all_products = cursor.execute("SELECT id, title, description, price FROM Products").fetchall()
+    return all_products
+
+
+connection.commit()
---
+++ Module 14 - Библиотека для работы с данными/4. План написания админ панели/Практика/module_14_4.py
@@ -0,0 +1,61 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+from crud_functions import *
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+kb1 = ReplyKeyboardMarkup(resize_keyboard=True)
+button1 = KeyboardButton(text='Информация')
+button2 = KeyboardButton(text='Купить')
+kb1.add(button1)
+kb1.add(button2)
+
+kb2 = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text='Продукт 1', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Продукт 2', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Продукт 3', callback_data='product_buying')],
+        [InlineKeyboardButton(text='Продукт 4', callback_data='product_buying')]
+    ]
+)
+
+all_products = get_all_products()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот, помогающий покупать наши товары.', reply_markup=kb1)
+
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer('Информация о боте!')
+
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    for i in range(4):
+        with open(f"products/{i + 1}.png", "rb") as img:
+            await message.answer(f"Название: {all_products[i][1]} | Описание: {all_products[i][2]} "
+                                 f"| Цена: {all_products[i][3]}")
+            await message.answer_photo(img)
+    await message.answer('Выберите продукт для покупки:', reply_markup=kb2)
+
+
+@dp.callback_query_handler(text='product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer('Введите команду /start для открытия меню.')
+
+
+executor.start_polling(dp, skip_updates=True)
