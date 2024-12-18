3a4ab121c49e4a05669115bf0b93f113fcbb560d
tr0lyaa <tolik2501@gmail.com>
2024-12-18 08:18:03 +0500

    Первый вариант работы

---
+++ Module 17 - Библиотека для работы с ресурсами. Часть 2/05. Использование БД в маршрутизации. Часть 2/Практика/app/routers/task.py
@@ -0,0 +1,36 @@
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from app.backend.db_depends import get_db
+from typing import Annotated
+from app.models import Task
+from app.schemas import CreateTask, UpdateTask
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
+
+router = APIRouter(prefix="/task", tags=["task"])
+
+
+@router.get("/")
+async def all_task(db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task)).all()
+    return tasks
+
+
+@router.get("/task_id")
+async def task_by_id():
+    pass
+
+
+@router.post("/create")
+async def create_task():
+    pass
+
+
+@router.put("/update")
+async def update_task():
+    pass
+
+
+@router.delete("/delete")
+async def delete_task():
+    pass
